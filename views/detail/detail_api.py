a1f704d397c8bbe361307e1b108a8cfaa3be7b25
sladuf <04fud27@gmail.com>
2022-04-05 15:09:54 +0900

    feat: detail api return json

--- views/detail/detail_api.py
+++ views/detail/detail_api.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request, abort
+from flask import Blueprint, request, abort, jsonify
 import os
+import requests
 
 bp = Blueprint("detail-api", __name__, url_prefix = "/detail/api")
 
@@ -10,28 +11,32 @@ def subdomain():
     if target == None:
         abort(400, description = "Parameter 'target' must be needed.")
 
+    data = []
+    result = []
     os.system('assetfinder '+target+ ' > result.txt')
+    f = open("result.txt","r")
+    while True:
+        line = f.readline()
+        if line == '':
+            break
+        data.append(line.strip())
+    
+    for i in data:
+        url = i
+
+        try:
+            res = requests.get("http://"+url, timeout=1)
+            result.append({'url' : i, 'status' : str(res)})
+        except requests.exceptions.Timeout as e:
+            result.append({'url' : i, 'status' : "Timeout"})
+        except requests.ConnectionError as e2:
+            result.append({'url' : i, 'status' : "NoResponse"})
+        
+
+    f.close()
 
     ## TODO
     ## subdomain 목록 가져오기
     return {
-        "result" : [
-            {
-                "site" : "test.com",
-                "status_code" : 200
-            },
-            {
-                "site" : "test.com",
-                "status_code" : 200
-            },
-            {
-                "site" : "test.com",
-                "status_code" : 200
-            },
-            {
-                "site" : "test.com",
-                "status_code" : 200
-            },
-
-        ]
+        "result" : result
     }

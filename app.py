7941a719056ba09ebdc29a52fc7ee6fc6230dae1
Universe1122 <whdals7979@gmail.com>
2022-04-06 01:04:58 +0900

    packet class 구현 완료

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from views.index import index
 from views.index import index_api
 from views.detail import detail
 from views.detail import detail_api
+from views.analyze import analyze_api
 from db.connect import createDatabase
 
 app = Flask(__name__, static_url_path = "", static_folder = "static", template_folder = "templates")
@@ -32,6 +33,7 @@ if __name__ == "__main__":
     app.register_blueprint(detail.bp)
     app.register_blueprint(detail_api.bp)
     app.register_blueprint(index_api.bp)
+    app.register_blueprint(analyze_api.bp)
     createDatabase()
 
     app.config["SAVE_DIR_PATH"] = "/home/universe/Desktop/git/proxify/logs/"
---
+++ views/analyze/analyze_api.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, abort, jsonify, request, current_app
+import os
+
+from views.func import getFolderNames, getFileNames
+from views.analyze.packet import Packet
+
+
+bp = Blueprint("analyze", __name__, url_prefix = "/analyze/api")
+
+
+@bp.route("/wappalyzer", methods=["GET"])
+def wappalyzer():
+    
+    target = request.args.get("target")
+
+    if not target:
+        return {
+            "result" : "error",
+            "message" : "target 파라미터가 비어 있습니다."
+        }, 400
+    
+    target_list = getFolderNames(current_app.config["SAVE_DIR_PATH"])
+
+    if not target in target_list:
+        return {
+            "result" : "error",
+            "message" : "{target} 폴더가 존재하지 않습니다.".format(target = target)
+        }, 400
+    
+    target_folder = current_app.config["SAVE_DIR_PATH"] + target
+    log_list = getFileNames(target_folder)
+
+    ## TODO
+    ## wappalyzer 기능 동작하는 함수 호출
+
+    return "1"
+
+
+@bp.route("/packet", methods=["GET"])
+def packet():
+
+    target = request.args.get("target")
+
+    if not target:
+        return {
+            "result" : "error",
+            "message" : "target 파라미터가 비어 있습니다."
+        }, 400
+    
+    target_list = getFolderNames(current_app.config["SAVE_DIR_PATH"])
+
+    if not target in target_list:
+        return {
+            "result" : "error",
+            "message" : "{target} 폴더가 존재하지 않습니다.".format(target = target)
+        }, 400
+
+    target_folder = current_app.config["SAVE_DIR_PATH"] + target
+    log_list = getFileNames(target_folder)
+
+    for filename in log_list:
+        with open(os.path.join(target_folder, filename)) as data:
+            packet = Packet(data.read())
+        break
+    
+    return "1"

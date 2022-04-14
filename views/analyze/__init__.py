90c405566ad33a0ebcd5cc02ea8227f66375adf7
Universe1122 <whdals7979@gmail.com>
2022-04-14 13:33:33 +0900

    packet 분석 범위 지정

--- views/analyze/__init__.py
+++ views/analyze/__init__.py
@@ -37,10 +37,79 @@ def fileMonitoring(SAVE_DIR_PATH, target_site, share_memory):
                     continue
 
                 packet = Packet(packet_data, regex_result)
-                analyze_obj.start(packet, file_name, target_folder)
+                if checkContentType(packet):
+                    analyze_obj.start(packet, file_name, target_folder)
 
         share_memory[target_site] = {
             "wappalyzer" : analyze_obj.wappalyzer_obj.wappalyer_result,
             "attack_vector" : analyze_obj.attack_vector_obj.attack_vector_result,
             "packet_count" : prev_file_count
         }
+
+
+def checkContentType(packet) -> bool:
+
+    ## Reference: https://developer.mozilla.org/ko/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+    filter_content_type_list = [
+        "audio/aac",
+        "application/x-abiword",
+        "application/octet-stream",
+        "video/x-msvideo",
+        "application/vnd.amazon.ebook",
+        "application/octet-stream",
+        "application/x-bzip",
+        "application/x-bzip2",
+        "application/x-csh",
+        "text/csv",
+        "application/msword",
+        "application/epub+zip",
+        "image/x-icon",
+        "image/gif",
+        "text/calendar",
+        "application/java-archive",
+        "image/jpeg",
+        "video/mpeg",
+        "audio/midi",
+        "application/vnd.apple.installer+xml",
+        "application/vnd.oasis.opendocument.presentation",
+        "application/vnd.oasis.opendocument.spreadsheet",
+        "application/vnd.oasis.opendocument.text",
+        "audio/ogg",
+        "video/ogg",
+        "application/ogg",
+        "application/pdf",
+        "application/vnd.ms-powerpoint",
+        "application/x-rar-compressed",
+        "application/rtf",
+        "application/x-sh",
+        "image/svg+xml",
+        "application/x-shockwave-flash",
+        "application/x-tar",
+        "image/tiff",
+        "application/x-font-ttf",
+        "application/vnd.visio",
+        "audio/x-wav",
+        "audio/webm",
+        "video/webm",
+        "image/webp",
+        "application/x-font-woff",
+        "application/xhtml+xml",
+        "application/vnd.ms-excel",
+        "application/xml",
+        "application/vnd.mozilla.xul+xml",
+        "application/zip",
+        "video/3gpp",
+        "video/3gpp2",
+        "application/x-7z-compressed"
+    ]
+
+    ## response header에 Content-Type 키가 없으면, attack_vector 검사 해야 함.
+    if not "Content-Type" in packet.response["header"].keys():
+        return True
+
+    for content_type in filter_content_type_list:
+        ## filter 할 content_type이 있으면, attack_vector 분석 안함.
+        if content_type in packet.response["header"]["Content-Type"]:
+            return False
+    
+    return True
